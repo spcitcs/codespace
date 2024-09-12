53a1c5b2e53cbe36ddd874025c0d9c12a36a9d68
Alfian Badrul Isnan <49936990+alfianisnan26@users.noreply.github.com>
2024-09-12 23:16:41 +0700

    re

--- .devcontainer/installer/dart.sh
+++
@@ -1,8 +0,0 @@
-curl -fsSL https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/dart-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/dart-archive-keyring.gpg] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main" | tee /etc/apt/sources.list.d/dart_stable.list > /dev/null
-apt-get update
-apt-get install -y --no-install-recommends dart
-git clone https://github.com/vickumar1981/jupyter-dart-kernel.git /opt/jupyter-dart-kernel
-cd /opt/jupyter-dart-kernel && pip install .
-jupyter kernelspec install /opt/jupyter-dart-kernel
-ln -s /usr/bin/dart /usr/local/bin/dart
--- README.md
+++ README.md
@@ -2,27 +2,6 @@
 
 Deskripsi terkait template Codespace untuk SPCITCS
 
-## Bahasa Program yang sudah terinstall:
-
-- [x] python
-- [x] dart
-- [ ] flutter
-- [ ] java22
-- [ ] c/c++
-- [ ] nodejs
-- [ ] golang
-- [ ] arduino
-
-## Aplikasi yang sudah terinstall:
-
-- [ ] jupyter (untuk python dan dart)
-- [ ] postgresql (database)
-- [ ] redis (cache)
-- [ ] minio (storage)
-- [ ] firebase
-- [ ] supabase
-- [ ] wowki (arduino simulator)
-
 # Setup
 
 ## Cara Melakukan Fork
